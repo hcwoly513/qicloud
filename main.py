6729d4109a02b81d7b6d81130134124115e70fb7
hcwoly513 <hcwoly513@gmail.com>
2015-03-23 13:07:28 +0800

    add session support.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8
 #!/usr/bin/env python
+# Name: main.py
+# Author: Chen-Wei Hung
+# Created Time: 2015-03-23
+# Updated Time: 2015-03-23
+# Copyright:   (c) PaulX 2015
 
 import os.path
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-
 
 from tornado.options import define, options
 import motor
