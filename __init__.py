25dcfbfdd9f2ba9beb34271816e5943d0b504b2a
hcwoly513 <hcwoly513@gmail.com>
2015-04-21 15:10:52 +0800

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8
+#!/usr/bin/env python3.4
+# Name: admin.py
+# Author: Chen-Wei Hung
+# Created Time: 2015-03-23
+# Updated Time: 2015-03-23
+# Copyright:   (c) PaulX 2015
+
+from __future__ import absolute_import, division, print_function, with_statement
+
+version = '0.1'
+version_info = (0, 1, 0, 0)
